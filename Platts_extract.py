a74caae0eccf7d4dd6ded4eb7e06855ca7826ed4
BIGboss248 <aminjamali79@gmail.com>
2022-12-24 13:09:56 +0330

    export to excel

--- Platts_extract.py
+++ Platts_extract.py
@@ -8,20 +8,21 @@ from tkinter import filedialog
 import pandas as pd
 
 
-def find_commodity_price_row(Platts_String: str, commodity_symbol: str,second_pattern='\s+\d+.+'):
+def find_commodity_price_row(Platts_String: str, commodity_symbol: str, second_pattern='\s+\d+.+'):
     """ Finds the row corosponding to the commodity inside the daily report and returns a re.match"""
     # the pattern is based on the symbol so in the program what the  function will recive is the symbol
     # corosponding to the needed commodity
-    pattern = "".join([rf'{commodity_symbol}',second_pattern])
+    pattern = "".join([rf'{commodity_symbol}', second_pattern])
     compiled_pattern = re.compile(pattern)
     matches = re.search(pattern=compiled_pattern, string=Platts_String)
     return matches
 
 
-def extract_numbers(Platts_String: str, commodity_symbol: str,second_pattern='\s+\d+.+'):
+def extract_numbers(Platts_String: str, commodity_symbol: str, second_pattern='\s+\d+.+'):
     """gets the file  and commodity symbol and uses the find_commodity_price_row gets the row of the commodity inside the daily report and
     removes the name symbol and spaces of the row and retruns a list of floats containing only the numbers meaning prices and changes"""
-    out_match = find_commodity_price_row(Platts_String, commodity_symbol,second_pattern)
+    out_match = find_commodity_price_row(
+        Platts_String, commodity_symbol, second_pattern)
     # creating a list of english letters to remove the name and symbol
     alphabet = list(string.ascii_letters)
     # extracting the string from re,match and turning it into a list of strings
@@ -51,20 +52,20 @@ def extract_numbers(Platts_String: str, commodity_symbol: str,second_pattern='\s
     return in_list
 
 
-def generate_report(Platts_String: str, commodity: dict,second_pattern='\s+\d+.+'):
+def generate_report(Platts_String: str, commodity: dict, second_pattern='\s+\d+.+'):
     """ taking the generated string from reading platts file and taking a list of required commodity information and Using
     1-find_commodity_price_row 2-extract_numbers it generates a list of lists containing commodity name, price and changes """
     result = []
     for i in list(commodity.keys()):
-        numbers = extract_numbers(Platts_String, commodity[i],second_pattern)
+        numbers = extract_numbers(Platts_String, commodity[i], second_pattern)
         numbers.insert(0, i)
         result.append(numbers)
     return (result)
 
 
-def final_report(Platts_String: str, commodity: dict, index: int, headers: list,second_pattern='\s+\d+.+'):
+def final_report(Platts_String: str, commodity: dict, index: int, headers: list, second_pattern='\s+\d+.+'):
     """Using generate_report it takes the output list and extracts the numbers we need plus adding the headers for pandas data farme"""
-    report = generate_report(Platts_String, commodity,second_pattern)
+    report = generate_report(Platts_String, commodity, second_pattern)
     result = []
     for i in report:
         temp = []
@@ -103,8 +104,7 @@ def get_Volume_Issue_Date(Platts_String: str):
 
 
 # declare addresses
-tkinter.Tk().withdraw()  # prevents an empty tkinter window from appearing
-Platts_file_full_address = filedialog.askopenfilename()
+Platts_file_full_address = r'G:\Shared drives\Unlimited Drive\Scripts\1-Global_Resourses\Platts-text.txt'
 
 # Open Platts file
 Platts_Daily_Report_File = open(
@@ -201,6 +201,17 @@ df_Asia_Pacific_brand_relativities_Low_Vol_HCC = pd.DataFrame(final_report(
     Platts_Daily_Report_String, Asia_Pacific_brand_relativities_Low_Vol_HCC, 2, ['Commodity', 'Price']))
 
 df_Dry_bulk_freight_assessments = pd.DataFrame(final_report(
-    Platts_Daily_Report_String, Dry_bulk_freight_assessments, 3, ['Commodity', 'Price', 'Change'],second_pattern='.+'))
+    Platts_Daily_Report_String, Dry_bulk_freight_assessments, 3, ['Commodity', 'Price', 'Change'], second_pattern='.+'))
 
-print(df_Dry_bulk_freight_assessments)
+# List of data frames for exporting to excel file
+dataframe_dict = {'indexes': df_indexes, 'lump': df_lump, 'pellet': df_pellet, 'ore_brands': df_ore_brands,
+                  'Asia_Pacific_coking_coal': df_Asia_Pacific_coking_coal,
+                  'Asia_Pacific_brand_relativities_Premium_Low_Vol': df_Asia_Pacific_brand_relativities_Premium_Low_Vol,
+                  'Asia_Pacific_brand_relativities_Low_Vol_HCC': df_Asia_Pacific_brand_relativities_Low_Vol_HCC,
+                  'Dry_bulk_freight_assessments': df_Dry_bulk_freight_assessments}
+
+excel_file_address = r'G:\Shared drives\Unlimited Drive\Global trading\Platts-Daily-Report\Platts-Data-V2.xlsx'
+excel_writer = pd.ExcelWriter(excel_file_address,engine='openpyxl')
+for dataframe in dataframe_dict:
+    dataframe_dict[dataframe].to_excel(excel_writer, sheet_name=dataframe)
+excel_writer.close()
